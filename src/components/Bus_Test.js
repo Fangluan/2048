e7839c6a6d97a8d52f9705d42c3673d921278948
r1 <protostomien@yahoo.fr>
2018-05-31 13:33:39 +0200

    Tentative de faire fonctionner un event à travers le bus (marche pas)

--- src/components/Bus_Test.js
+++ src/components/Bus_Test.js
@@ -2,12 +2,19 @@ import React, { Component } from 'react';
 import bus from '../bus'
 
 class BusTest extends Component {
-    onTestCall = ({message}) => {
+    emitTestCall = ({message}) => {
         bus.emit('Test_call', message)
     }
-
+    onTestCall = ({message}) => {
+        bus.on('Test_call', message)
+    }
     render() {
-        return <button onClick={this.onTestCall}>Test</button>
+        return (
+            <div>Div qui traine</div>,
+            <button onClick={this.emitTestCall("toto")}>Test</button>,
+            <div>{this.onTestCall}</div>
+            
+        )
     }
 }
 
